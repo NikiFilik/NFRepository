7dad05cade503740104ecee468438d1d1c688a1c
Никита Филатов <nikfilik@mail.ru>
2024-01-25 10:46:43 +0200

    lab1 done

--- lab1_functions.cpp
+++ lab1_functions.cpp
@@ -1,1 +1,17 @@
 #include "lab1_functions.hpp"
+
+namespace nf {
+	void ReadingFile(std::ifstream& in, int& launch_num) {
+		std::string trash;
+		while (!in.eof()) {
+			in >> launch_num;
+			std::getline(in, trash);
+		}
+		in.close();
+	}
+
+	void OutputToFile(std::ofstream& out, int launch_num, struct tm* my_date) {
+		out << launch_num + 1 << "-" << my_date->tm_year + 1900 << "-" << my_date->tm_mon + 1 << "-" << my_date->tm_mday << "-" << my_date->tm_hour << "-" << my_date->tm_min << "-" << my_date->tm_sec << std::endl;
+		out.close();
+	}
+}
--- lab1_main.cpp
+++ lab1_main.cpp
@@ -7,21 +7,16 @@
 
 int main(){
 	
-	time_t mytime = time(NULL);
-	struct tm *mydate = localtime(&mytime);
+	time_t my_time = time(NULL);
+	struct tm *my_date = localtime(&my_time);
 	
 	std::ifstream in("LaunchTime.txt");
-	int LaunchNum = 0;
-	std::string trash;
-	while(!in.eof()){
-		in >> LaunchNum;
-		std::getline(in, trash);
-	}
-	in.close();
+	int launch_num = 0;
+
+	nf::ReadingFile(in, launch_num);
 	
 	std::ofstream out("LaunchTime.txt", std::ios::app);
-	out << LaunchNum + 1 << "-" << mydate->tm_year + 1900 << "-" << mydate->tm_mon + 1 << "-" << mydate->tm_mday << "-" << mydate->tm_hour << "-" << mydate->tm_min << "-" << mydate->tm_sec << std::endl;
-	out.close();
+	nf::OutputToFile(out, launch_num, my_date);
 	
 	return 0;
 }
