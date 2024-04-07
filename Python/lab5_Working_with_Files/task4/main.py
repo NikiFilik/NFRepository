41e3f784f895b023582348b7c1d84d1acb0dcc03
Nikita Filatov <91692887+NikiFilik@users.noreply.github.com>
2024-04-07 21:49:56 +0200

    Add files via upload

--- Python/lab5_Working_with_Files/task4/main.py
+++ Python/lab5_Working_with_Files/task4/main.py
@@ -1,30 +1,14 @@
 import json
-
+import csv
 
 
 jsonFile = open("Sample-employee-JSON-data.json", "r")
-
 data = json.load(jsonFile)
-
 jsonFile.close()
 
 
-
-csvFile = open("Sample-employee-CSV-data.csv", "w")
-
-for i in data["Employees"][0].keys():
-    csvFile.write(i)
-    if i != "emailAddress":
-        csvFile.write(",")
-    else:
-        csvFile.write("\n")
-
-for i in data["Employees"]:
-    for j in i.keys():
-        csvFile.write(i[j])
-        if j != "emailAddress":
-            csvFile.write(",")
-        else:
-            csvFile.write("\n")
-
+csvFile = open("Sample-employee-CSV-data.csv", "w", newline = "")
+writer = csv.DictWriter(csvFile, data["Employees"][0].keys())
+writer.writeheader()
+writer.writerows(data["Employees"])
 csvFile.close()
