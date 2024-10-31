85223d8d6e0ec5f24b9f49554dfe887cfb269cd9
Nikita Filatov <91692887+NikiFilik@users.noreply.github.com>
2024-10-31 11:31:35 +0200

    Add files via upload

--- Algorithms and Data Structures/lab12/main.cpp
+++ Algorithms and Data Structures/lab12/main.cpp
@@ -39,35 +39,101 @@ void readInputData(int chunkLen) {
 	std::ofstream outA("tempA.txt", std::ios::out);
 	std::ofstream outB("tempB.txt", std::ios::out);
 
+	char currentOut = 'A';
+	int* arr = new int[chunkLen];
+	
 	while (!in.eof()) {
-		int *arr = new int[chunkLen];
 		int i = 0;
-		while (!in.eof()) {
-			if (i < chunkLen) {
-				arr[i];
-				i++;
+		while (!in.eof() && i < chunkLen) {
+			in >> arr[i];
+			i++;
+		}
+		quickSort(arr, 0, i - 1);
+
+		if (currentOut == 'A' && !in.eof()) {
+			int n = i;
+			for (i = 0; i < n; i++) {
+				outA << arr[i] << std::endl;
 			}
+			currentOut = 'B';
+		}
+		else if (currentOut == 'B' && !in.eof()){
+			int n = i;
+			for (i = 0; i < n; i++) {
+				outB << arr[i] << std::endl;
+			}
+			currentOut = 'A';
 		}
 	}
-	CreateRuns(S)
-		S размер создаваемых отрезков
-		CurrentFile = A
-		while конец входного файла не достигнут do
-			read S записей из входного файла
-			sort S записей
-			if CurrentFile = A then
-				CurrentFile = B
-			else
-				CurrrentFile = A
-				end if
-				end while
+	delete[] arr;
+}
+
+void polyphaseMerge(int chunkLen) {
+	int size = chunkLen;
+	char input1 = 'A';
+	char input2 = 'B';
+	char currentOutput = 'C';
+
+	while (true /*ХЗ, БРО, НАДО ДУМАТЬ*/) {
+		std::ifstream in1;
+		std::ifstream in2;
+
+		std::ofstream out1;
+		std::ofstream out2;
+		if (input1 == 'A' && input2 == 'B') {
+			in1.open("tempA.txt", std::ios::in);
+			in2.open("tempB.txt", std::ios::in);
+
+			out1.open("tempC.txt", std::ios::out);
+			out2.open("tempD.txt", std::ios::out);
+		}
+		if (input1 == 'C' && input2 == 'D') {
+			in1.open("tempC.txt", std::ios::in);
+			in2.open("tempD.txt", std::ios::in);
+
+			out1.open("tempA.txt", std::ios::out);
+			out2.open("tempB.txt", std::ios::out);
+		}
+
+		//Данный while, получается, переливает из одной пары файлов в другую, увеличивая размер отрезочков в два раза
+		while (!in1.eof() && !in2.eof()) {
+			//СЛИЯНИЕ ДВУХ ОТРЕЗОЧКОВ ДЛИНЫ size
+
+			if (currentOutput == 'A') {
+				currentOutput = 'B';
+			}
+			else if (currentOutput == 'B') {
+				currentOutput = 'A';
+			}
+			else if (currentOutput == 'C') {
+				currentOutput = 'D';
+			}
+			else if (currentOutput == 'D') {
+				currentOutput = 'C';
+			}
+		}
+
+		size *= 2;
+		if (input1 == 'A' && input2 == 'B') {
+			input1 = 'C';
+			input2 = 'D';
+			currentOutput = 'A';
+		}
+		else if(input1 == 'C' && input2 == 'D'){
+			input1 = 'A';
+			input2 = 'B';
+			currentOutput = 'C';
+		}
+	}
 }
 
 int main() {
 	srand(time(NULL));
 	setlocale(LC_ALL, "Russian");
 
-	generateInputData(10000, 0, 10000);
+	generateInputData(100, 0, 100);
+
+	readInputData(25);
 
 	return 0;
 }
