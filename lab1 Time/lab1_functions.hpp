cd31ab77d04fd080c686a788f2ea99f8ceed2577
Nikita Filatov <91692887+NikiFilik@users.noreply.github.com>
2025-02-17 17:23:38 +0200

    Delete lab1 Time directory

--- lab1 Time/lab1_functions.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <fstream>
-#include <string>
-
-namespace nf {
-	void ReadingFile(std::ifstream& in, int& launch_num);
-
-	void OutputToFile(std::ofstream& out, int launch_num, struct tm* my_date);
-}
